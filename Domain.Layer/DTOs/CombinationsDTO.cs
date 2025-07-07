a5705bffeb26ccb561ae6829f3ff4e8a73b74f99
DanielWilches <danielprietowilches@gmail.com>
2025-07-07 15:57:11 -0500

    PARTE 1 - Correccion algoritmo

--- Domain.Layer/DTOs/CombinationsDTO.cs
+++ Domain.Layer/DTOs/CombinationsDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Layer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,9 @@ namespace Domain.Layer.DTOs
 {
     public class CombinationsDTO
     {
-        public List<string> Strateys { get; set; }
-        public List<string> IdStrateys { get; set; }
-        public int? totalLabs { get; set; }
-        public double? avgPerformance { get; set; }
-        public double? avgConsumption { get; set; }
+        public int TotalLabs { get; set; }
+        public List<int>? AvgPerformance { get; set; }
+        public List<double>? AvgConsumption { get; set; }
+        public List<TiresModel> Strateys { get; set; } = new();
     }
 }
